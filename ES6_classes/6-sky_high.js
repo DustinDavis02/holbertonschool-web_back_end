80882257175b862eb66488496e8bddfeb8228d1a
Dustin Davis <davisdustin328@gmail.com>
2023-05-17 19:09:01 -0500

    Pass lint

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,7 +3,7 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    
+
     if (typeof floors !== 'number') {
       throw TypeError('floors must be a number');
     }
