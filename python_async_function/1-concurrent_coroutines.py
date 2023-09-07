8d6bff332c511781beefa8e0e6467072c142abf3
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:18:00 -0500

    Fixing random delay

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ from importlib import import_module
 module = import_module('0-basic_async_syntax')
 wait_random = module.wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Wait for n seconds, then return a list of delays"""
     tasks = [wait_random(max_delay) for _ in range(n)]
