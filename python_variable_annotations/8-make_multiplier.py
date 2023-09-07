9462eeffbf51249116c0a6b058a09a0dda0c2713
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 14:04:23 -0500

    Multiplies float by a number

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,1 +1,9 @@
 #!/usr/bin/env python3
+"""Return another function that multiplies a float by a number."""
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by a number."""
+    def multipler_func(n: float) -> float:
+        return n * multiplier
+    return multipler_func
