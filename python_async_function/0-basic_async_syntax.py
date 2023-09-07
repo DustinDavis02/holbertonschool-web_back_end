eb270f225dbf3a3941dc1b92bf15c7b9323011e2
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 15:29:00 -0500

    asynchronous coroutine waits for delay and returns

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+"""This module contains an asynchronous coroutine that waits for a random delay and returns it."""
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> int:
+    """Wait for a random delay"""
+    delay = random.randint(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
