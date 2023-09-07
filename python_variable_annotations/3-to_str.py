79facfbb9e5e62dcb609003b9fb62326a13c24d4
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 12:36:51 -0500

    Fixing pycodestyle

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-"""Function that takes a float as an argument and returns the string representation of the float"""
+"""Function that takes a float as an argument
+    and returns the string representation 
+                of the float"""
 
 
 def to_str(n: float) -> str:
