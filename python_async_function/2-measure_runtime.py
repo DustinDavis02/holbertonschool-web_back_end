b9915e807cf9aa00c37ebe9574c99fc61b303f37
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:12:01 -0500

    Measure total execution time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Function to measure total execution time."""
+import time
+import asyncio
+from typing import list
+from importlib import import_module
+
+module = import_module('1-concurrent_runtime')
+wait_n = module.wait_random
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measure total execution time."""
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    
+    total_time = end_time - start_time
+    return total_time / n
