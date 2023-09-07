c9136c1597a139534f5263754a99c375c11da16e
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 12:37:29 -0500

    Whitespace

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Function that takes a float as an argument
-    and returns the string representation 
+    and returns the string representation
                 of the float"""
 
 
