1aaa7f6590fe8eba278f8ce51e8daf60a5d8c90c
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:28:13 -0500

    pycodestyle

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""This module contains an asynchronous coroutine that waits for a random delay and returns it."""
+"""asynchronous coroutine waits for a delay and returns it."""
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """Wait for a random delay"""
