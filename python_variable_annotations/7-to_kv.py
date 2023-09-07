b2aed4670c60206012c785c9de1e5fd0ff965de6
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 13:56:41 -0500

    Return Tuple and square

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Function that returns a tuple of a string and the square of a number"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Function that returns a tuple of a string and the square of a number"""
+    return (k, float(v ** 2))
