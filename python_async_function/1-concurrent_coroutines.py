152d037920475b42f271290567869b055cb31de3
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:04:23 -0500

    Return list of delays

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Asynchronous coroutine that runs wait_random multiple times"""
+import asyncio
+from typing import List
+from importlib import import_module
+
+module = import_module('0-basic_async_syntax')
+wait_random = module.wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Wait for n seconds, then return a list of delays"""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
