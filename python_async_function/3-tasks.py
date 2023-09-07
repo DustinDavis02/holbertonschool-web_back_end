b0b53216f2c6e2f20808e4d1636186918b186644
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:57:54 -0500

    Pycodestyle

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,7 @@ from importlib import import_module
 module = import_module('0-basic_async_syntax')
 wait_random = module.wait_random
 
+
 def task_wait_random(max_delay: int) -> asyncio.Task:
     """function that creats an asyncio.Task."""
     return asyncio.create_task(wait_random(max_delay))
