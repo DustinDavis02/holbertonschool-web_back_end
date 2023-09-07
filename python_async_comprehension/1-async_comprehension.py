691b88cbe9ff8f9f078b29beec2807f0d4bcafcf
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 17:40:09 -0500

    Return 10 numbers

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""function for async_comprehension"""
+import asyncio
+from typing import List
+from importlib import import_module
+
+module = import_module('0-async_generator')
+async_generator = module.async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """async_comprehension"""
+    return [i async for i in async_generator()]
