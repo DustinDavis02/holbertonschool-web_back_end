75122ec2b754621cf88881740c3ccf0253d9ae2f
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 14:11:42 -0500

    Fixing all pycodestyle

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """Function that returns a tuple of a string and the square of a number"""
 from typing import Tuple, Union
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Function that returns a tuple of a string and the square of a number"""
     return (k, float(v ** 2))
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """Return another function that multiplies a float by a number."""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Return a function that multiplies a float by a number."""
     def multipler_func(n: float) -> float:
