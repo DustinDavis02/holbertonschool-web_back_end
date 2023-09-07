a1dfe99428cae9f17aa9fcad17accbe38079d599
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:57:12 -0500

    Grammar errors

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -6,6 +6,6 @@ from importlib import import_module
 module = import_module('0-basic_async_syntax')
 wait_random = module.wait_random
 
-def task_wait_random(max_delax: int) -> asyncio.Task:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """function that creats an asyncio.Task."""
-    return asyncio.create_task(wait_random(max_delax))
+    return asyncio.create_task(wait_random(max_delay))
