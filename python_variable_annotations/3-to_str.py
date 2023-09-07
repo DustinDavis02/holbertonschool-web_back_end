7e0a6b9123dcc9208cdd73d7d926e5d5bf6f19fe
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 12:26:57 -0500

    Float as argument to string representation

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Function that takes a float as an argument and returns the string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of the float"""
+    return str(n)
