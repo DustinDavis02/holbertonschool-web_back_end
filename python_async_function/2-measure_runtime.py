71f8d12f91e9ce6d9befe9a1ad0d38590544ead8
Dustin Davis <davisdustin328@gmail.com>
2023-09-07 16:36:44 -0500

    Fixing how to get elapse time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -10,9 +10,7 @@ wait_n = module.wait_random
 
 def measure_time(n: int, max_delay: int) -> float:
     """Measure total execution time."""
-    start_time = time.time()
+    start: float = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    
-    total_time = end_time - start_time
-    return total_time / n
+    end: float = time.time()
+    return end - start
