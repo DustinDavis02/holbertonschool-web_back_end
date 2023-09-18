ab873c7574ba2816227145084219e6800c1522a6
Dustin Davis <davisdustin328@gmail.com>
2023-09-17 19:47:16 -0500

    LIFOCache

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+""" LIFOCache module """
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class
+    Inherits from BaseCaching and is a caching system implementing LIFO algorithm """
+    
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value for the key key """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+        if key in self.keys:
+            self.keys.remove(key)
+        self.keys.append(key)
+
+        if len(self.keys) > BaseCaching.MAX_ITEMS:
+            discarded_key = self.keys.pop(-2)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+    def get(self, key):
+        """ Return the value in self.cache_data linked to key """
+        return self.cache_data.get(key, None)
