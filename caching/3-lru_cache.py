27fc15695fe30bc0519c17b1dc40ab92f3c23cf1
Dustin Davis <davisdustin328@gmail.com>
2023-09-17 19:47:24 -0500

    LRUCache

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,1 +1,37 @@
 #!/usr/bin/env python3
+""" LRUCache module """
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """ LRUCache class
+    Inherits from BaseCaching and is a caching system implementing LRU algorithm """
+    
+    def __init__(self):
+        """ Initialize """
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        """ Assign to the dictionary self.cache_data the item value for the key key """
+        if key is None or item is None:
+            return
+
+        if key in self.keys:
+            self.keys.remove(key)
+        elif len(self.keys) >= BaseCaching.MAX_ITEMS:
+            discarded_key = self.keys.pop(0)
+            del self.cache_data[discarded_key]
+            print(f"DISCARD: {discarded_key}")
+
+        self.keys.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Return the value in self.cache_data linked to key """
+        if key is None or key not in self.cache_data:
+            return None
+        self.keys.remove(key)
+        self.keys.append(key)
+        return self.cache_data[key]
