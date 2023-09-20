b6b9d9233dc954a0c9826484763f551e26788ed1
Dustin Davis <davisdustin328@gmail.com>
2023-09-20 18:28:41 -0500

    Obfuscated

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This module is designed to obfuscate specific fields in a log message."""
+
+import re
+
+def filter_datum(fields: list, redaction: str, message: str, separator: str) -> str:
+    """
+    Obfuscates specified fields.
+    
+    Parameters:
+        fields (list): Fields to be obfuscated.
+        redaction (str): String to replace field values.
+        message (str): Original log message.
+        separator (str): Character that separates each field in the log line.
+        
+    Returns:
+        str: The log message with specified fields obfuscated.
+    """
+    return re.sub(fr'(?<=\b{separator.join(fields)}=)([^{separator}]*)', redaction, message)
+
+if __name__ == "__main__":
+    pass
