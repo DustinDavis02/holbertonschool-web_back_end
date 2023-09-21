55b44f7cd50c5b8d69e299a64b5eb042dc2f18a7
Dustin Davis <davisdustin328@gmail.com>
2023-09-20 20:54:24 -0500

    routes authentication

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -13,13 +13,35 @@ class Auth:
     """
     Auth Class
 
-    This class is responsible for managing authentication
-    for all your API endpoints.
+    class is responsible for managing authentication
+    for all API endpoints.
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Method to require authentication. Returns False for now. """
-        return False
+        """
+        Method to require authentication based on the path and excluded_paths.
+        
+        Parameters:
+            - path (str): The path from the request
+            - excluded_paths (List[str]): List of string paths that don't need authentication
+
+        Returns:
+            bool: True if authentication is required, False if not
+        """
+    
+        # Case 1: path is None
+        if path is None:
+            return True
+        
+        # Case 2: excluded_paths is None or empty
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        
+        # Make method slash-tolerant
+        path = path.rstrip('/')
+        
+        # Case 3: path is in excluded_paths
+        return not any(ex_path.rstrip('/') == path for ex_path in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header. Returns None for now. """
