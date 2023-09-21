28264ac904ce46023b3f382fc253abf404e5d28f
Dustin Davis <davisdustin328@gmail.com>
2023-09-20 21:43:48 -0500

    Base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@ This is the BasicAuth module.
 This module is responsible for handling Basic API authentication.
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
@@ -16,7 +17,7 @@ class BasicAuth(Auth):
     for all API endpoints.
     """
     def extract_base64_authorization_header(self,
-                                            authorization_header: str)-> str:
+                                            authorization_header: str) -> str:
         """
         Extracts the Base64 encoded part of the authorization header.
 
@@ -42,3 +43,31 @@ class BasicAuth(Auth):
 
         # Extract and return the Base64 encoded part
         return authorization_header.split(" ")[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                               str) -> str:
+        """
+        Decodes Base64 encoded part of the authorization header.
+
+        Parameters:
+            base64_authorization_header (str): Base64 encoded part of the
+            Authorization header.
+
+        Returns:
+            str: Decoded Base64 string as UTF8 if valid, otherwise None.
+        """
+
+        # Check base64_authorization_header is None
+        if base64_authorization_header is None:
+            return None
+
+        # Check base64_authorization_header is not a string
+        if not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            # Decode Base64 string and return as UTF8
+            return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
