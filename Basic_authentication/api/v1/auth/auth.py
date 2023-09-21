f356282bb50f507cd1f3cea025b0e97bf9b1c579
Dustin Davis <davisdustin328@gmail.com>
2023-09-20 20:56:03 -0500

    pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -20,7 +20,7 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Method to require authentication based on the path and excluded_paths.
-        
+
         Parameters:
             - path (str): The path from the request
             - excluded_paths (List[str]): List of string paths that don't need authentication
@@ -32,16 +32,17 @@ class Auth:
         # Case 1: path is None
         if path is None:
             return True
-        
+
         # Case 2: excluded_paths is None or empty
         if excluded_paths is None or len(excluded_paths) == 0:
             return True
-        
+
         # Make method slash-tolerant
         path = path.rstrip('/')
-        
+
         # Case 3: path is in excluded_paths
-        return not any(ex_path.rstrip('/') == path for ex_path in excluded_paths)
+        return not any(ex_path.rstrip('/')
+                       == path for ex_path in excluded_paths)
 
     def authorization_header(self, request=None) -> str:
         """ Method to get the authorization header. Returns None for now. """
