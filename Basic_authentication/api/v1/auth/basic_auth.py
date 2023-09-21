4e43982bd660dbb5a7979337327b5f0e508dcd9f
Dustin Davis <davisdustin328@gmail.com>
2023-09-20 22:06:09 -0500

    style fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -73,8 +73,10 @@ class BasicAuth(Auth):
         except Exception:
             return None
 
-    def extract_user_credentials(self,
-                             decoded_base64_authorization_header: str) -> (str, str):
+    def extract_user_credentials(
+        self,
+        decoded_base64_authorization_header: str
+    ) -> (str, str):
 
         """
         Extracts user email and password from the decoded Base64.
