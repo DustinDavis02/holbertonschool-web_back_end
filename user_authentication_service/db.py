66875dff92ee703cc2e91f4beecf3286e0e5d218
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 18:55:17 -0500

    Pycode style

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -38,7 +38,7 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
-    
+
     def find_user_by(self, **kwargs) -> User:
         """
         Finds a user by given keyword arguments.
