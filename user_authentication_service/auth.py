1cc734176b675b25c6523f446a8c8d352c3e94c1
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 19:51:16 -0500

    over-indented error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -53,16 +53,16 @@ class Auth:
             return None
 
     def get_user_from_session_id(self, session_id: str):
-            """
-            Retrieve the user corresponding to a session ID.
-            """
-            if session_id is None:
-                return None
+        """
+        Retrieve the user corresponding to a session ID.
+        """
+        if session_id is None:
+            return None
 
-            try:
-                return self._db.find_user_by(session_id=session_id)
-            except NoResultFound:
-                return None
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
 
 
 def _generate_uuid() -> str:
