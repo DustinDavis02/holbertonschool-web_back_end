8905c6d3a845d3c197129c6dc3120002ae8b5e84
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 21:28:18 -0500

    Session cookie

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -4,7 +4,7 @@ This is the Auth module.
 
 This module is responsible for handling API authentication.
 """
-
+from os import getenv
 from typing import List, TypeVar
 from flask import request
 
@@ -63,3 +63,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method to get the current user. Returns None for now. """
         return None
+    
+    def session_cookie(self, request=None):
+        """ Returns none. """
+        if request is None:
+            return None
+
+        session_name = getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
