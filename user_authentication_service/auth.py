43497c2150c928053dfbd459b5836ddfc028d349
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 19:17:52 -0500

    Pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -12,20 +12,25 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def _hash_password(password: str) -> bytes:
-        """
-        Hashes password string and returns salted hash bytes.
-        """
-        salt = bcrypt.gensalt()
-        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-        return hashed
-
     def register_user(self, email: str, password: str) -> User:
         """
         Registers a user with a given email and password.
         """
         try:
+            # Check if user exists
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            return self._db.add_user(email, _hash_password(password))
+            # Hash password
+            hashed_password = _hash_password(password)
+            # Add user to database
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes password string and returns salted hash bytes.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
