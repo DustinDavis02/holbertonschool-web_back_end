d3ac627b41ce0a47043e1fad52b49124714995be
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 19:24:25 -0500

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,9 +3,11 @@
 This module sets up a basic Flask app with a single GET route.
 """
 
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from auth import Auth
 app = Flask(__name__)
+
+AUTH = Auth()
 
 
 @app.route('/', methods=['GET'])
@@ -14,6 +16,20 @@ def welcome():
     GET route that returns a JSON welcome message.
     """
     return jsonify({"message": "Bienvenue"}), 200
+
+@app.route('/users', methods=['POST'])
+def users():
+    """
+    POST route for registering users.
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
