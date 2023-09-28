a04b361b9e556d08a4ef8a25677e31d9952284e5
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 19:38:31 -0500

    pycodestyle

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -21,7 +21,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
             return bcrypt.checkpw(password.encode('utf-8')
-                                , user.hashed_password)
+                                 , user.hashed_password)
         except NoResultFound:
             return False
 
@@ -42,7 +42,8 @@ class Auth:
 
 
 def _generate_uuid() -> str:
-    """ Returns a string representation of a new UUID.
+    """ 
+    Returns a string representation of a new UUID.
     """
     return str(uuid.uuid4())
 
