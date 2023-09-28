58a0bfe818970839e9223c1be3c165ced0d14eb4
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 19:10:14 -0500

    register new user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,12 +1,36 @@
 #!/usr/bin/env python3
 """This module contains utility functions for authentication."""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
-def _hash_password(password: str) -> bytes:
-    """
-    Hashes password string and returns salted hash bytes.
-    """
-    salt = bcrypt.gensalt()
-    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed
+    def __init__(self):
+        self._db = DB()
+
+    def _hash_password(password: str) -> bytes:
+        """
+        Hashes password string and returns salted hash bytes.
+        """
+        salt = bcrypt.gensalt()
+        hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Registers a user with a given email and password.
+        """
+        try:
+            # Check if user exists
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            # Hash password
+            hashed_password = _hash_password(password)
+            # Add user to database
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
