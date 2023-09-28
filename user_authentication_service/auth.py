0a4dc71104ac33227a3c3e38b76d6830b09521a9
Dustin Davis <davisdustin328@gmail.com>
2023-09-27 19:34:57 -0500

    Generate UUIDs

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@ import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 class Auth:
@@ -19,7 +20,8 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8')
+                                  , user.hashed_password)
         except NoResultFound:
             return False
 
@@ -38,6 +40,11 @@ class Auth:
             new_user = self._db.add_user(email, hashed_password)
             return new_user
 
+
+def _generate_uuid() -> str:
+    """ Returns a string representation of a new UUID.
+    """
+    return str(uuid.uuid4())
 
 def _hash_password(password: str) -> bytes:
     """
