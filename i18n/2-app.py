70fbf1b7f7c0f845686f7473c7b97dd2eeb7f4a2
Dustin Davis <davisdustin328@gmail.com>
2023-10-04 18:01:22 -0500

    pycodestyle

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -23,6 +23,7 @@ def get_locale():
     """Determine the best match for supported languages."""
     return request.accept_languages.best_match(Config.LANGUAGES)
 
+
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Main route"""
