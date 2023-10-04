684fbf3b1f5594838262b84011779f41b9486a6f
Dustin Davis <davisdustin328@gmail.com>
2023-10-04 18:41:13 -0500

    force locale

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -2,7 +2,7 @@
 """Parametrize templates"""
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel, _, force_locale
 
 
 class Config:
@@ -14,20 +14,15 @@ class Config:
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
 babel = Babel(app)
-
-
-@babel.localeselector
-def get_locale():
-    """Determine the best match for supported languages."""
-    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Main route"""
-    return render_template('3-index.html', home_title=_("home_title"), home_header=_("home_header"))
+    best_match = request.accept_languages.best_match(["en", "fr"])
+    with force_locale(best_match):
+        return render_template('3-index.html', home_title=_("home_title"), home_header=_("home_header"))
 
 
 if __name__ == '__main__':
