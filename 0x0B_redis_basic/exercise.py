993c1f546e076c3883c207e5ff8e17df02e6a958
Dustin Davis <davisdustin328@gmail.com>
2023-10-17 15:56:13 -0500

    Remove test

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,10 +18,3 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-if __name__ == "__main__":
-    cache = Cache()
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
