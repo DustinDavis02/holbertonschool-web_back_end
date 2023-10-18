038f2df6b2c0168ba84342bf0643bb666761ac3d
Dustin Davis <davisdustin328@gmail.com>
2023-10-17 20:11:59 -0500

    list all bands with "Glam rock"

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,13 @@
+-- List all bands with Glam rock as their main style, ranked by their longevity
+SELECT
+    band_name,
+    (CASE 
+        WHEN split = 'Present' THEN YEAR(CURDATE()) - formed
+        ELSE split - formed
+    END) AS lifespan
+FROM
+    metal_bands
+WHERE
+    main_style = 'Glam rock'
+ORDER BY
+    lifespan DESC;
