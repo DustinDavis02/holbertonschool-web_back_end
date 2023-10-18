c5653d77a53ef3bc157ac4703d9448af9b9d243e
Dustin Davis <davisdustin328@gmail.com>
2023-10-17 20:13:20 -0500

    edits

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- List all bands with Glam rock as their main style, ranked by their longevity
+-- List bands with Glam rock as main style, ranked by longevity
 SELECT
     band_name,
     (IFNULL(split, 2020) - formed) AS lifespan
